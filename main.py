403eb25542305e38d883abcd92d96e67b8128487
ildar <ganiyev.ildar@gmail.com>
2022-07-01 21:24:55 +0300

    Use if __main__

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ from email.message import EmailMessage
 from datetime import datetime
 from dateutil import relativedelta
 
-print("Script started")
 
-months = relativedelta.relativedelta(datetime.now(), datetime(2022, 7, 1)).months
-msg = EmailMessage()
+if __name__ == "__main__":
+    print("Script started")
 
-msg.set_content(f'''{os.getenv("FIO")}
-{os.getenv("ADDRESS")}
-Лицевой счет {os.getenv("ACCOUNT")}
-Кухня:
-ГВС - {44 + months * 2}
-ХВС - {123 + months * 2}
-Сан. узел:
-ГВС - {101 + months * 2}
-ХВС - {261 + months * 2}
- 
-89О4676O579''')
+    months = relativedelta.relativedelta(datetime.now(), datetime(2022, 7, 1)).months
+    msg = EmailMessage()
 
-msg['Subject'] = os.getenv("ADDRESS")
-msg['From'] = os.getenv("mail-mine")
-msg['To'] = os.getenv("mail-send")
+    msg.set_content(f'''{os.getenv("FIO")}
+    {os.getenv("ADDRESS")}
+    Лицевой счет {os.getenv("ACCOUNT")}
+    Кухня:
+    ГВС - {44 + months * 2}
+    ХВС - {123 + months * 2}
+    Сан. узел:
+    ГВС - {101 + months * 2}
+    ХВС - {261 + months * 2}
 
-print("Message prepared")
+    89О4676O579''')
 
-# Send the message via our own SMTP server.
-server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
-server.login(os.getenv("mail-mine"), os.getenv("psw"))
-server.send_message(msg)
-print("message sent")
-server.quit()
-print("server quit")
+    msg['Subject'] = os.getenv("ADDRESS")
+    msg['From'] = os.getenv("mail-mine")
+    msg['To'] = os.getenv("mail-send")
+
+    print("Message prepared")
+
+    # Send the message via our own SMTP server.
+    server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+    server.login(os.getenv("mail-mine"), os.getenv("psw"))
+    server.send_message(msg)
+    print("message sent")
+    server.quit()
+    print("server quit")
