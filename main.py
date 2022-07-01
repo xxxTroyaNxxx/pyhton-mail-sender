d77dd3e7e1b514d63a781d1e328defa13c48ce51
ildar <ganiyev.ildar@gmail.com>
2022-07-01 13:57:12 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from datetime import datetime
+from dateutil import relativedelta
+
+months = relativedelta.relativedelta(datetime.now(), datetime(2022, 7, 1)).months
+msg = EmailMessage()
+
+msg.set_content(f'''{os.getenv("FIO")}
+{os.getenv("ADDRESS")}
+Лицевой счет {os.getenv("ACCOUNT")}
+Кухня:
+ГВС - {44 + months * 2}
+ХВС - {123 + months * 2}
+Сан. узел:
+ГВС - {101 + months * 2}
+ХВС - {261 + months * 2}
+ 
+89О4676O579''')
+
+msg['Subject'] = os.getenv("ADDRESS")
+msg['From'] = os.getenv("mail-mine")
+msg['To'] = os.getenv("mail-send")
+
+# Send the message via our own SMTP server.
+server = smtplib.SMTP_SSL('smtp.yandex.com', 465)
+server.login(os.getenv("mail-mine"), os.getenv("psw"))
+server.send_message(msg)
+server.quit()
