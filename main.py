696ba5f9f2272e4ee7f79f0d138a0e7dc99ea3a7
ildar <ganiyev.ildar@gmail.com>
2022-07-01 21:12:25 +0300

    Add logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from email.message import EmailMessage
 from datetime import datetime
 from dateutil import relativedelta
 
+print("Script started")
+
 months = relativedelta.relativedelta(datetime.now(), datetime(2022, 7, 1)).months
 msg = EmailMessage()
 
@@ -23,8 +25,12 @@ msg['Subject'] = os.getenv("ADDRESS")
 msg['From'] = os.getenv("mail-mine")
 msg['To'] = os.getenv("mail-send")
 
+print("Message prepared")
+
 # Send the message via our own SMTP server.
 server = smtplib.SMTP_SSL('smtp.yandex.com', 465)
 server.login(os.getenv("mail-mine"), os.getenv("psw"))
 server.send_message(msg)
+print("message sent")
 server.quit()
+print("server quit")
